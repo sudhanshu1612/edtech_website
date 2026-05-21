139c7b3eee526fcce7933f627ab9d239004222fc
sudhanshu <rsudhanshu1612@gmail.com>
2026-05-21 18:22:25 +0530

    Initial commit

---
+++ server/models/OTP.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
+
+const OTPSchema = new mongoose.Schema({
+    email:
+    {
+        type:String,
+        required: true,
+    },
+    otp: 
+    {
+        type:String,
+        required:true,
+    },
+    createdAt:
+    {
+        type: Date,
+        default: Date.now(),
+        expires: 5*60,
+    }
+});
+
+//a function to send mail
+async function sendVerificationEmail(email,otp) 
+{
+    try
+    {
+        const mailResponse = await mailSender(email , "verification email from study notion " , otp);
+        console.log("email sent successfully",mailResponse);
+    }
+    catch(error)
+    {
+        console.log("error occured while sending mail : ", error);
+        throw error;
+    }
+}
+
+//before saving OTP to the database send the mail to the client
+OTPSchema.pre("save" , async function(next)
+{
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
+
+
+module.exports = mongoose.model("OTP",OTPSchema);
