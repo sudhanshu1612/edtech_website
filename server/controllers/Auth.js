53679b4849a87fe29f16322e10a46603513ca230
sudhanshu <rsudhanshu1612@gmail.com>
2026-05-22 15:47:41 +0530

    new commit

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
 
 //sendOTP
 
@@ -72,88 +73,106 @@ const otpGenerator = require("otp-generator");
 //signup
 exports.signUp = async (req , res) => 
 {
-    //data fetch from req ki body
-    const 
+    try
     {
+    //data fetch from req ki body
+        const 
+        {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,  //will get one value already student or instuctor
+            contactNumber,
+            otp
+        } = req.body;
+        //validate
+        if(!firstName || !lastName || !password || !confirmPassword || !otp)
+        {
+            return res.status(403).json({
+                success: false,
+                message:"All fields are required",
+            })
+        }
+        // match the password second password
+        if(password !== confirmPassword)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "password and confirm password does not match please try again"
+            });
+        }
+        //check if user already exist
+        const existUser = await User.findOne({email});
+        if(existUser)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "user is already registered",
+            });
+        }
+        //find most recent otp
+        const recentOtp = await OTP.find({email}).sort({createdAt: -1}).limit(1);
+        console.log(recentOtp);
+        //validate otp
+        if(recentOtp.length == 0) 
+        {
+            //otp not found
+            return res.status(400).json({
+                success: false,
+                message: "OTP not found",
+            })
+        }
+        else if(otp != recentOtp)
+        {
+            //invalid otp
+            return res.status(400).json({
+                success:false,
+                message:"otp entered is incorrect",
+            })
+        }
+        //hash passaword
+        const hashedPassword = await bcrypt.hash(password , 10);
+        //entry create in db
+
+        const profileDetails = await Profiler.create (
+        {
+            gender:null,
+            dateOfBirth: null,
+            about: null,
+            contactNumber: null,
+        });
+
+        const user = await user.create(
+        {
         firstName,
         lastName,
         email,
-        password,
-        confirmPassword,
-        accountType,  //will get one value already student or instuctor
         contactNumber,
-        otp
-    } = req.body;
-    //validate
-    if(!firstName || !lastName || !password || !confirmPassword || !otp)
-    {
-        return res.status(403).json({
-            success: false,
-            message:"All fields are required",
+        password:hashedPassword,
+        accountType,
+        additionalDetails: profileDetails._id,
+        image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstname} ${lastName}`,
         })
-    }
-    // match the password second password
-    if(password !== confirmPassword)
-    {
-        return res.status(400).json({
-            success: false,
-            message: "password and confirm password does not match please try again"
+        //return res
+        return res.status(200).json(
+        {
+            success: true,
+            message:'User is registered successfully',
+            user,
         });
     }
-    //check if user already exist
-    const existUser = await User.findOne({email});
-    if(existUser)
+    catch(error)
     {
-        return res.status(400).json({
+        console.log(error);
+        return res.status(500).json({
             success: false,
-            message: "user is already registered",
-        });
-    }
-    //find most recent otp
-    const recentOtp = await OTP.find({email}).sort({createdAt: -1}).limit(1);
-    console.log(recentOtp);
-    //validate otp
-    if(recentOtp.length == 0) 
-    {
-        //otp not found
-        return res.status(400).json({
-            success: false,
-            message: "OTP not found",
+            message:'User cannot be registered please try again',
         })
     }
-    else if(otp != recentOtp)
-    {
-        //invalid otp
-        return res.status(400).json({
-            success:false,
-            message:"otp entered is incorrect",
-        })
-    }
-    //hash passaword
-    const hashedPassword = await bcrypt.hash(password , 10);
-    //entry create in db
-
-    const profileDetails = await Profiler.create (
-    {
-        gender:null,
-        dateOfBirth: null,
-        about: null,
-        contactNumber: null,
-    });
-
-    const user = await user.create(
-    {
-       firstName,
-       lastName,
-       email,
-       contactNumber,
-       password:hashedPassword,
-       accountType,
-       additionalDetails: profileDetails._id,
-       image: 
-    })
-    //return res
 }
+
 //login
 
 //change password
