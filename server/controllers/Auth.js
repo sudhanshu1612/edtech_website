c398f470a0e1058bc4b7ebbf5e7d2de65cd3affe
sudhanshu <rsudhanshu1612@gmail.com>
2026-05-22 14:54:02 +0530

    new commit

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,4 +1,24 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+
+//sendOTP
+exports.sendOTP = async (req ,res) => {
+
+    // fetch email from request ki body
+    const {email} = req.body;
+
+    //check if user already exist 
+    const checkUserPresent = await User.findOne({email});
+
+    //if user already exists then return a  response
+    if(checkUserPresent) {
+        return res.status(401).json({
+            success: false,
+            message:"user already present",
+        })
+    }
+}
 //signup
 //login
-//sendOTP
+
 //change password
