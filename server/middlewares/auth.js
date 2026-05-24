159617a61cbab77d4d0d021b55e528f93bac3832
sudhanshu <rsudhanshu1612@gmail.com>
2026-05-24 13:55:02 +0530

    new commit

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -7,14 +7,115 @@ exports.auth = async ( req , res , next) =>
 {
     try
     {
-       
+       //extrct token 
+       const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer ", "");
+
+       //if missing token return response
+       if(!token)
+       {
+          return res.status(401).json(
+            {
+                success: false,
+                message: 'Token is missing',
+            })
+       }
+       //verify the token
+       try
+       {
+          const decode = jwt.verify(token , process.env.JWT_SECRET);
+          console.log(decode);
+          req.user = decode;
+       }
+       catch(error)
+       {
+           //issue in verification
+           res.status(401).json(
+            {
+                success: false,
+                message: 'Token is invalid',
+            })
+       }
+       next();
     }
     catch (error)
     {
-
+        res.status(401).json(
+            {
+                success: false,
+                message: 'Something went wrong while validating the token',
+            })
     }
 }
 //isstudent
+exports.isStudent = async (req ,res , next) =>
+{
+   try
+   {
+      if(req.user.accountType !== "Student")
+      {
+        return res.status(401).json(
+            {
+                success: false,
+                message:"this is protected route for students only",
+            }
+        )}
+    next();
+   }
+   catch(error)
+   {
+      res.status(401).json(
+        {
+            success: false,
+            message: 'User role cannot be verified',
+        })
+   }
+}
 //isinstructor
+exports.isinstructor = async (req ,res , next) =>
+{
+   try
+   {
+      if(req.user.accountType !== "Instructor")
+      {
+        return res.status(401).json(
+            {
+                success: false,
+                message:"this is protected route for instructor only",
+            }
+        )}
+    next();
+   }
+   catch(error)
+   {
+      res.status(401).json(
+        {
+            success: false,
+            message: 'User role cannot be verified',
+        })
+   }
+}
 //isAdmin
+exports.isAdmin = async (req ,res , next) =>
+{
+   try
+   {
+      if(req.user.accountType !== "Admin")
+      {
+        return res.status(401).json(
+            {
+                success: false,
+                message:"this is protected route for Admin only",
+            }
+        )}
+    next();
+   }
+   catch(error)
+   {
+      res.status(401).json(
+        {
+            success: false,
+            message: 'User role cannot be verified',
+        })
+   }
+}
 
