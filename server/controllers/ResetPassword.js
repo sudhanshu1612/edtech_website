6ea0fc42440f0203d065aa66569a81c2a34f0e6f
sudhanshu <rsudhanshu1612@gmail.com>
2026-05-24 14:26:54 +0530

    new commit

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,3 +1,29 @@
-//reset password token
-//reset password
+const User = require("../models/USer");
+const mailSender = require("../utils/mailSender");
 
+//reset password token
+exports.resetPasswordToken = async (req , res) => 
+{
+   //get email from req.body
+   const email = req.body.email;
+   //check user for this email
+   const user = await User.findOne({email: email});
+   if(!user)
+   {
+    return res.json(
+    {
+        success: false,
+        message:'your email is not registered with us',
+    })
+   }
+   //generate token
+   const token = 
+   //update user by adding token and expiration time
+   //create url
+   //send mail containing the url
+   //return response
+
+   const url = `http://localhost:3000/udate-password/${token}`
+
+}
+//reset password
