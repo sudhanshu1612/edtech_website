6a2c4e0c5a1a6ef676e7f6fd2dce51e06841182d
sudhanshu <rsudhanshu1612@gmail.com>
2026-05-24 14:37:20 +0530

    new commit

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -4,26 +4,52 @@ const mailSender = require("../utils/mailSender");
 //reset password token
 exports.resetPasswordToken = async (req , res) => 
 {
-   //get email from req.body
-   const email = req.body.email;
-   //check user for this email
-   const user = await User.findOne({email: email});
-   if(!user)
+   try
    {
-    return res.json(
-    {
+        //get email from req.body
+        const email = req.body.email;
+        //check user for this email
+        const user = await User.findOne({email: email});
+        if(!user)
+        {
+            return res.json(
+            {
+                success: false,
+                message:'your email is not registered with us',
+            })
+        }
+        //generate token
+        const token = crypto.randomUUID();
+        //update user by adding token and expiration time
+        const updatedDetails = await User.findOneAndUpdate(
+                                {email:email},
+                                {
+                                token: token,
+                                resetPasswordExpires: Date.now() + 5*60*1000,
+                                },
+                                {new:true}, //updated document will be returned in response with new:true
+                            );
+        //create url
+        const url = `http://localhost:3000/udate-password/${token}`
+        //send mail containing the url
+        await mailSender(email,"Password Reset Link",
+                                `Password Reset Link: ${url}`
+        );
+        //return response
+        return res.json(
+            {
+                success: true,
+                message: "email sent successfully , please check email and change password",
+            }
+        );
+        }
+   catch(error)
+   {
+       console.log(error);
+       return res.status(500).json({
         success: false,
-        message:'your email is not registered with us',
-    })
+        message: "mail can't be sent try again",
+       })
    }
-   //generate token
-   const token = 
-   //update user by adding token and expiration time
-   //create url
-   //send mail containing the url
-   //return response
-
-   const url = `http://localhost:3000/udate-password/${token}`
-
 }
 //reset password
