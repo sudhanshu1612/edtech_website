4fc11e2a0ed1e960f23c13c2e3ff818883ddc263
sudhanshu <rsudhanshu1612@gmail.com>
2026-06-02 08:52:45 +0530

    firstApp

--- server/controllers/SubSection.js
+++ server/controllers/SubSection.js
@@ -39,8 +39,13 @@ exports.createSubSection = async (req , res) => {
             },
             {new:true},
          );
+         //log update secgtion details after adding populate entry
          //return response
-
+         return res.status(200).json({
+            success: true,
+            message: 'SubSection created successfully',
+            updatedSectionDetails,
+         })
     }
     catch(error)
     {
@@ -49,5 +54,78 @@ exports.createSubSection = async (req , res) => {
             message: 'Unable to create subsection ,please try again',
             error: error.message,
          })
+    }
+}
+
+//HWupdae Subsection
+exports.updateSubSection = async (req , res) => {
+    try
+    {       //data input
+            const {subSectionId , title , timeDuration , description } = req.body;
+            //data validation
+            if(!subSectionId || !title || !timeDuration || !description)
+            {
+                return res.status(400).json({
+                    success: false,
+                    message: 'Missing one or more properties',
+                });
+            }
+            //update data
+            const updatedSubSection = await SubSection.findByIdAndUpdate(
+                subSectionId,
+                {
+                    title: title,
+                    timeDuration: timeDuration,
+                    description: description,
+                },
+                {new:true},
+            );  
+            //return response
+            return res.status(200).json({
+                success: true,
+                message: 'SubSection updated successfully',
+                updatedSubSection,
+            })
+    }
+    catch(error)
+    {
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to update subsection please try again',
+            error: error.message,
+        });
+    }
+}
+
+
+
+//delete subsection
+exports.deleteSubSection = async (req , res) => {
+    try
+    {   //get Id --assuming that we are sending id in params
+        const {subSectionId}= req.params;
+        //data validation
+        if(!subSectionId)
+        {
+            return res.status(400).json({
+                success: false,
+                message:'SubSection Id is required',
+            });
+        }
+        //delete data
+        await SubSection.findByIdAndDelete(subSectionId);
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: 'SubSection deleted successfully',
+        })
+    }
+    catch(error)
+    {
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to delete subsection please try again',
+            error: error.message,
+        });
     }
 }
