fcf3eebea3409c3cdef5d1681c81e5d7317576b3
sudhanshu <rsudhanshu1612@gmail.com>
2026-06-02 08:29:18 +0530

    firstApp

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -46,13 +46,33 @@ exports.createSection = async (req , res ) =>
     }
 }
 
-exports.exportSection = async (req , res) => {
+exports.updateSection = async (req , res) => {
     try
     {
-       
+       //data input
+       const {sectionName , sectionId} = req.body;
+       //data validation
+       if(!sectionName || !sectionId)
+       {
+        return res.status(400).json({
+            success: false,
+            message: 'Missing Properties',
+        });
+       }
+       //update data
+       const section = await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true});
+       //return response
+       return res.status(200).json({
+        success: true,
+        message:'Section Updated Successfully',
+       });
     }
     catch(error)
     {
-
+        return res.status(500).json({
+            success: false,
+            message:'Unable to update section please try again',
+            error: error.message,
+        });
     }
 }
