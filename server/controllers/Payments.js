94dbd0981d71a61bd0b04d16c21fa3c438e30ad3
sudhanshu <rsudhanshu1612@gmail.com>
2026-06-03 15:15:53 +0530

    firstApp

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -1,1 +1,15 @@
-const instance =
+const {instance} = require("../config/razorpay");
+const Course  = require("../models/Course");
+const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
+const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail");
+
+//capture the payment and initaiate the razorpay order
+exports.capturePayment = async (req , res) => {
+    //get course id and user id
+    //validation
+    //valid courseId
+    //uer already pay for the same course
+    //order create 
+    // return response
+}
